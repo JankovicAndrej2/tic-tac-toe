cf358d3838665452941cfbc355cbd5a3a43f6842
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2023-11-08 22:04:32 +0100

    Dodani efekti na pocetnoj strani

---
+++ script/first-site-script.js
@@ -0,0 +1,63 @@
+let choosePlayers = document.querySelectorAll(".players");
+
+let playersOption;
+
+
+choosePlayers.forEach((element,index) => {
+    element.addEventListener("click", ()=>{
+        if(index == 1){
+            choosePlayers[1].style.textShadow = "0px 0px 13px rgb(11, 148, 228)";
+            choosePlayers[0].style.textShadow = "0px 0px 0px rgb(11, 148, 228)";
+            playersOption = index;
+        }
+        else if(index == 0)
+        {
+            choosePlayers[0].style.textShadow = "0px 0px 13px rgb(11, 148, 228)";
+            choosePlayers[1].style.textShadow = "0px 0px 0px rgb(11, 148, 228)";
+            playersOption = index;
+        }
+    })
+    element.addEventListener("mouseover", ()=>{
+        element.style.textShadow = "0px 0px 13px rgb(11, 148, 228)";
+    })
+    element.addEventListener("mouseout", ()=>{
+        if(!(index == playersOption))
+        {
+            element.style.textShadow = "0px 0px 0px rgb(11, 148, 228)";
+        }
+    })
+
+})
+
+
+
+let chooseSign = document.querySelectorAll(".signs");
+let signOptions;
+
+chooseSign.forEach((element,index) => {
+    element.addEventListener("click", ()=>{
+        if(index == 1){
+            chooseSign[1].style.textShadow = "0px 0px 13px rgb(11, 148, 228)";
+            chooseSign[0].style.textShadow = "0px 0px 0px rgb(11, 148, 228)";
+            playersOption = index;
+        }
+        else if(index == 0)
+        {
+            chooseSign[0].style.textShadow = "0px 0px 13px rgb(11, 148, 228)";
+            chooseSign[1].style.textShadow = "0px 0px 0px rgb(11, 148, 228)";
+            playersOption = index;
+        }
+    })
+    element.addEventListener("mouseover", ()=>{
+        element.style.textShadow = "0px 0px 13px rgb(11, 148, 228)";
+    })
+    element.addEventListener("mouseout", ()=>{
+        if(!(index == playersOption))
+        {
+            element.style.textShadow = "0px 0px 0px rgb(11, 148, 228)";
+        }
+    })
+
+})
+
+
